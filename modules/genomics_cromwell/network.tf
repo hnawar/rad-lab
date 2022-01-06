28e16218abb42643db3dd9c81cf57f0b229824b6
Hatem Nawar <hnawar@google.com>
2022-01-06 23:30:46 +0000

    Add license to .tf files
    Signed-off-by: Hatem Nawar <hnawar@google.com>

--- modules/genomics_cromwell/network.tf
+++ modules/genomics_cromwell/network.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 data "google_compute_network" "default" {
   count   = var.create_network ? 0 : 1
   project = local.project.project_id
@@ -64,7 +80,6 @@ module "vpc_cromwell" {
     google_project_service.enabled_services
   ]
 }
-
 
 
 module "cloud-nat" {
